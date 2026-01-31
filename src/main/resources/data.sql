34c42ae6c6be449bafff2f9cff3b9716676efb59
khasnhnq <quockhanh31032005@gmail.com>
2026-02-01 01:34:35 +0700

    Add H2 database and sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO students (id, name, age, email, gender) VALUES
+(1, 'Nguyen Van A', 20, 'a@gmail.com', 'Nam'),
+(2, 'Tran Thi B', 21, 'b@gmail.com', 'Nu'),
+(3, 'Le Van C', 19, 'c@gmail.com', 'Nam');
