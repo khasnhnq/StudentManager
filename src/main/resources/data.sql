504917b6f2adce3a1f883bf2ca7fdf1435b4a662
khasnhnq <quockhanh31032005@gmail.com>
2026-02-01 12:00:29 +0700

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO students (name, age, email, gender) VALUES
-('Nguyễn Văn A', 20, 'a@gmail.com', 'Nam'),
-('Trần Thị B', 21, 'b@gmail.com', 'Nữ'),
-('Lê Văn C', 19, 'c@gmail.com', 'Nam');
+INSERT INTO students (id, name, age, email, gender) VALUES
+(RANDOM_UUID(), 'Nguyễn Văn A', 20, 'a@gmail.com', 'Nam'),
+(RANDOM_UUID(), 'Trần Thị B', 21, 'b@gmail.com', 'Nữ'),
+(RANDOM_UUID(), 'Lê Văn C', 19, 'c@gmail.com', 'Nam');
